6303ef7d0c72be63a7692679a80b8a24937f2a35
Adal1942 <adautosombra1942@gmail.com>
2022-06-11 14:42:29 -0300

    projeto de profile

--- App.js
+++ App.js
@@ -1,20 +1,95 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Image, View, SafeAreaView, StatusBar, Text, Pressable, Linking} from 'react-native';
 
-export default function App() {
+const colorGithub = '#010409';
+const colorFontGithub = '#C9D1D9';
+const colorDarkFontGithub = '#4f565E';
+const imageProfileGithub = 'https://avatars.githubusercontent.com/u/65043062?v=4';
+const urlToMyGithub = 'https://github.com/Adal1942';
+
+const App = () => {
+
+  const handlePressGoToGithub = async ()=>{
+    const res = await Linking.canOpenURL(urlToMyGithub);
+    if (res) {
+       await Linking.openURL(urlToMyGithub);
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <SafeAreaView style={style.container}>
+      <StatusBar backgroundColor={colorGithub} barStyle= "light-content"/>
+      <View style={style.content}>
+        <Image 
+        accessibilityLabel="Adauto no shopping com segurando um milk-shake"
+        style={style.avatar} 
+        source={{uri: imageProfileGithub}}/>
 
-const styles = StyleSheet.create({
-  container: {
+        <Text 
+        accessibilityLabel="Nome: Adauto Nascimento"
+        style={[style.defaultText, style.name]}>Adauto Nascimento</Text>
+
+        <Text 
+        accessibilityLabel="Nickname: Adal1942"
+        style={[style.defaultText, style.nickname]}>Adal1942</Text>
+
+        <Text
+        accessibilityLabel="Descrição: Estudando e inventando novas coisas com programação."
+        style={[style.defaultText, style.description]}>Estudando e inventando novas coisas com programação.</Text>
+
+        <Pressable onPress={handlePressGoToGithub}>
+          <View style={style.button}>
+            <Text 
+            style={[style.defaultText, style.textButton]}>Open in Github</Text>
+          </View>
+        </Pressable>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+const style = StyleSheet.create({
+  container:{
+    backgroundColor: colorGithub,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+  },
+  content:{
+    alignItems: 'center',
+    padding: 20,
+  },
+  avatar:{
+    height: 200,
+    width: 200,
+    borderRadius: 100,
+    borderColor: 'white',
+    borderWidth: 2,
+  },
+  defaultText:{
+    color: colorFontGithub,
+  },
+  name:{
+    fontWeight: 'bold',
+    fontSize: 24,
+  },
+  nickname:{
+    fontSize: 18,
+    color: colorDarkFontGithub,
+  },
+  description:{
+    fontWeight: 'bold',
+    fontSize: 14,
+  },
+  button:{
+    backgroundColor: colorDarkFontGithub,
+    borderRadius: 10,
+    padding: 20,
+    marginTop: 20,
+  },
+  textButton:{
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 });
